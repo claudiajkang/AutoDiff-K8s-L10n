7dd243e285fd8b67cabb17a63fa5090b0ae0f81a
Claudia J.Kang <claudiajkang@gmail.com>
2020-05-13 14:33:04 +0900

    Update script for multiple file

--- create_diff.py
+++ create_diff.py
@@ -11,35 +11,36 @@ L10N_WORKING_BRANCH = git_pr_res['base']['ref']
 L10N_LANG = L10N_WORKING_BRANCH.split('-')[-1].split('.')[0]
 
 GIT_BASE_FILE_URL = f'{GIT_BASE_PR_URL}/files'
-git_res = requests.get(GIT_BASE_FILE_URL).json()
+git_all_res = requests.get(GIT_BASE_FILE_URL).json()
 
-KO_FILE_URL = git_res[0]['raw_url']
-EN_FILE_URL = git_res[0]['raw_url']\
-    .replace(git_res[0]['sha'], L10N_WORKING_BRANCH)\
-    .replace(f'/{L10N_LANG}', '/en')
+for git_res in git_all_res:
+    KO_FILE_URL = git_res['raw_url']
+    EN_FILE_URL = git_res['raw_url']\
+        .replace(git_res['sha'], L10N_WORKING_BRANCH)\
+        .replace(f'/{L10N_LANG}', '/en')
 
-NEW_FILE_NAME = git_res[0]['filename']\
-    .replace("content/ko/docs/", "")\
-    .replace("content/ko/", "")\
-    .replace('/', '-')
+    NEW_FILE_NAME = git_res['filename']\
+        .replace("content/ko/docs/", "")\
+        .replace("content/ko/", "")\
+        .replace('/', '-')
 
-curDir = os.getcwd()
-repo = Repo(curDir)
+    curDir = os.getcwd()
+    repo = Repo(curDir)
 
-PR_URL = GIT_BASE_PR_URL.replace('api.', '')\
-    .replace('/repos', '')\
-    .replace('pulls', 'pull')
+    PR_URL = GIT_BASE_PR_URL.replace('api.', '')\
+        .replace('/repos', '')\
+        .replace('pulls', 'pull')
 
-en_res = requests.get(EN_FILE_URL)
-f = open(f'{curDir}/{NEW_FILE_NAME}', "wb")
-f.write(en_res.content)
-f.close()
-repo.index.add(NEW_FILE_NAME)
-repo.index.commit(f'#{PR_NUM} : add original {NEW_FILE_NAME}\n\n- PR url : {PR_URL}\n- file url : {EN_FILE_URL}')
+    en_res = requests.get(EN_FILE_URL)
+    f = open(f'{curDir}/{NEW_FILE_NAME}', "wb")
+    f.write(en_res.content)
+    f.close()
+    repo.index.add(NEW_FILE_NAME)
+    repo.index.commit(f'#{PR_NUM} : add original {NEW_FILE_NAME}\n\n- PR url : {PR_URL}\n- file url : {EN_FILE_URL}')
 
-ko_res = requests.get(KO_FILE_URL)
-f = open(f'{curDir}/{NEW_FILE_NAME}', "wb")
-f.write(ko_res.content)
-f.close()
-repo.index.add(NEW_FILE_NAME)
-repo.index.commit(f'#{PR_NUM} : update translated {NEW_FILE_NAME}\n\n- PR url : {PR_URL}\n- file url : {KO_FILE_URL}')
+    ko_res = requests.get(KO_FILE_URL)
+    f = open(f'{curDir}/{NEW_FILE_NAME}', "wb")
+    f.write(ko_res.content)
+    f.close()
+    repo.index.add(NEW_FILE_NAME)
+    repo.index.commit(f'#{PR_NUM} : update translated {NEW_FILE_NAME}\n\n- PR url : {PR_URL}\n- file url : {KO_FILE_URL}')
